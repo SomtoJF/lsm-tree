8cc544392b0d8c8f0a1ca253fc8d4437311419fb
Somtochukwu Francis <somtofrancis5@gmail.com>
2026-09-02 02:17:07 +0100

    Initial Implementation

--- database/client.go
+++ database/client.go
@@ -4,7 +4,7 @@ import "github.com/SomtoJF/lsm-tree/database/somtodb"
 
 type Database interface {
 	Set(key int, value string) string
-	Get(key int) string
+	Get(key int) (string, error)
 }
 
 func NewDatabase(filePath string) Database {
--- database/somtodb/client.go
+++ database/somtodb/client.go
@@ -2,21 +2,27 @@ package somtodb
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 type SomtoDB struct {
 	filePath string
+	// maps keys to their corresponding value offsets in the file
+	indexes map[int]int
+	// number of bytes written to the file
+	fileSize int
 }
 
 func Init(filePath string) *SomtoDB {
 	db := &SomtoDB{}
 	db.filePath = filePath
+	db.fileSize = 0
 	return db
 }
 
-func (db *SomtoDB) write(data []byte) error {
-	f, err := os.OpenFile(db.filePath, os.O_APPEND|os.O_WRONLY, 0644)
+func (db *SomtoDB) write(key int, data []byte) error {
+	f, err := os.OpenFile(db.filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -26,22 +32,55 @@ func (db *SomtoDB) write(data []byte) error {
 	if err != nil {
 		return err
 	}
+
+	db.fileSize += len(data)
+	db.indexes[key] = db.fileSize
 	return nil
+}
+
+func (db *SomtoDB) read(offset int) ([]byte, error) {
+	f, err := os.Open(db.filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	_, err = f.Seek(int64(offset), 0)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, 100)
+	_, err = f.Read(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 func (db *SomtoDB) Set(key int, value string) string {
 	// TODO: implement
-	text := fmt.Sprintf("key: %d, value: %s", key, value)
+	text := fmt.Sprintf("key: %d, value: %s\n", key, value)
 	textbytes := []byte(text)
-	err := db.write(textbytes)
+	err := db.write(key, textbytes)
 	if err != nil {
+		log.Println(err)
 		return "Error: failed to write to file"
 	}
 	return text
 }
 
-func (db SomtoDB) Get(key int) string {
+func (db SomtoDB) Get(key int) (string, error) {
 	// TODO: implement
+	offset, ok := db.indexes[key]
+	if !ok {
+		return "", fmt.Errorf("key not found")
+	}
 
-	return "value"
+	readData, err := db.read(offset)
+	if err != nil {
+		return "", err
+	}
+	return string(readData), nil
 }
